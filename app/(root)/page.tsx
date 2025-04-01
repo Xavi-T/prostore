bdf982becf7b19ab7606aee55839032531c13c06
sontran <sontranvan@vnpt.vn>
2025-04-01 15:37:01 +0700

    S2-e16

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,13 @@
+import ProductList from '@/components/shared/product/product-list'
+import sampleData from '@/db/sample-data'
 export default function HomePage() {
-  return <div>hellow</div>
+  return (
+    <div>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </div>
+  )
 }
