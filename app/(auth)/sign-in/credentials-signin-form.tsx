e9e293c58dfccb16185d80fa550a1fd4e86929af
SonHip <sonhipkmhd@gmail.com>
2025-04-18 15:37:17 +0700

    feat(auth): add callback URL handling to sign-in form and page

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -6,6 +6,7 @@ import { Label } from '@/components/ui/label'
 import { signInWithCredentials } from '@/lib/actions/user.actions'
 import { singInDefaultValues } from '@/lib/constant'
 import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
 import { useActionState } from 'react'
 import { useFormStatus } from 'react-dom'
 
@@ -14,6 +15,9 @@ function CredentialsSignInForm() {
     message: '',
     success: false
   })
+
+  const searchParams = useSearchParams()
+  const callbackUrl = searchParams.get('callbackUrl') || '/'
 
   const SignInButton = () => {
     const { pending } = useFormStatus()
@@ -26,6 +30,7 @@ function CredentialsSignInForm() {
 
   return (
     <form action={action}>
+      <input type="text" hidden name="callbackUrl" defaultValue={callbackUrl} />
       <div className="space-y-6">
         <div>
           <Label htmlFor="email"></Label>
